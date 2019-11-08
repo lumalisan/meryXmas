aabd3820d578ee80dced43dc6eb55fa869dc2049
PussyDestroyer69 <lumalisan@gmail.com>
2019-11-08 21:01:29 +0100

    Barbara with

--- meryXmas.py
+++ meryXmas.py
@@ -24,7 +24,6 @@ class meryXmas(object):
 
     def reno(self):
         #Lo que esta dentro del with es la seccion critica
-        print("{} here!".format(nombreRenos[self.contadorRenos]))
         with self.mutex:
             self.contadorRenos += 1
             if self.contadorRenos == 9:
@@ -33,7 +32,9 @@ class meryXmas(object):
             else:
                 print("Reindeer {} arrives!".format(nombreRenos[self.contadorRenos - 1]))
         self.renoSem.acquire()
-        prepararReno(nombreRenos[self.contadorRenos - 1])
+        with self.mutex:
+            self.contadorRenos -= 1
+            prepararReno(nombreRenos[self.contadorRenos])
 
     def santa(self):
         print("-------> Santa says: I'm going to sleep")
@@ -41,17 +42,23 @@ class meryXmas(object):
         print("-------> Santa says: I'm awake ho ho ho!")
         with self.mutex:
             if self.contadorRenos == 9:
-                self.contadorRenos = 0
+                #self.contadorRenos = 0
                 prepararTrineo()
-                self.renoSem.release(9)
+                #self.renoSem.release(9)
+                for i in range(9):
+                    self.renoSem.release()
+                    self.contadorRenos -= 1
             else:
-                self.elfoSem.release(3)
+                #self.elfoSem.release(3)
+                for i in range(3):
+                    self.elfoSem.release()
                 ayudarElfos()
 
     def elfo(self):
         self.elfoMutex.acquire()
         with self.mutex:
             self.contadorElfos += 1
+            print("Elf {} says: I have a question, I'm the {} waiting...".format(nombreElfos[self.contadorElfos - 1], self.contadorElfos))
             if self.contadorElfos == 3:
                 self.santaSem.release()
             else:
@@ -74,6 +81,12 @@ def prepararTrineo():
     print("Santa loads the toys")
     print("-------> Santa says: Until next Christmas")
 
+def ayudarElfos():
+    print("PPEP")
+
+def pedirAyuda():
+    print("stephanie who S U C C S like a PPEP")
+
 
 
 # Creamos las funciones de los threads
@@ -82,10 +95,14 @@ def threadSanta(mxm):
     mxm.santa()
 
 def threadRenos(mxm):
+    print("{} here!".format(nombreRenos[self.contadorRenos]))
     mxm.reno()
 
 def threadElfos(mxm):
+    print("Hi I am the elf {}".format(nombreElfos[self.contadorElfos]))
     mxm.elfo()
+
+
 
 def main():
     threads = []
